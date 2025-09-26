33b4fcb9d33da7cf1faf44d0b97a52abaf867f79
y.ly <phuongduy2010@gmail.com>
2025-09-26 12:19:36 +0700

    handle cancel request

--- src/app/features/movies/components/movie-component/movie-component.ts
+++ src/app/features/movies/components/movie-component/movie-component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Movie } from '../../data-access/movie.model';
 import { MovieService } from '../../service.ts/movie.service';
+import { Subscription } from 'rxjs';
 
 
 
@@ -12,29 +13,39 @@ import { MovieService } from '../../service.ts/movie.service';
   templateUrl: './movie-component.html',
   styleUrl: './movie-component.css'
 })
-export class MovieComponent implements OnInit {
+export class MovieComponent implements OnInit, OnDestroy {
    searchTerm: string = '';
    movies: Array<Movie> = [];
    moviesState = signal<Movie[]>([]);
    loading = signal(false);
+   private lastSearchSub?: Subscription;
    constructor(private movieSvc: MovieService) {}
+    ngOnDestroy(): void {
+        this.unsubscribe();
+    }
     ngOnInit(): void {
         this.searchMovies('SpiderMan');
     }
-
-   searchMovies(title: string): void {
-        this.loading.set(true);
-        this.movieSvc.getMovies(title).subscribe(response => {
-            if(response.ok){
-                this.moviesState.set(response.data.items || []);
-            }
-            this.loading.set(false);
-        });
-   }
 
    onSearch(): void {
         if (this.searchTerm.trim()) {
             this.searchMovies(this.searchTerm);
         }
     }
+
+    private unsubscribe(): void {
+        this.lastSearchSub?.unsubscribe();
+    }
+
+    private searchMovies(title: string): void {
+        this.unsubscribe();
+        this.loading.set(true);
+        this.lastSearchSub = this.movieSvc.getMovies(title)
+        .subscribe(response => {
+            if(response.ok){
+                this.moviesState.set(response.data.items || []);
+            }
+            this.loading.set(false);
+        });
+   }
 }
--- src/app/features/movies/components/movie-detail-component/movie-detail-component.ts
+++ src/app/features/movies/components/movie-detail-component/movie-detail-component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MovieDetail } from '../../data-access/movie.model';
 import { MovieService } from '../../service.ts/movie.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movie-detail-component',
@@ -9,14 +10,25 @@ import { MovieService } from '../../service.ts/movie.service';
   templateUrl: './movie-detail-component.html',
   styleUrl: './movie-detail-component.css'
 })
-export class MovieDetailComponent implements OnInit {
+export class MovieDetailComponent implements OnInit, OnDestroy {
   movie: MovieDetail = {} as MovieDetail;
   movieState = signal<MovieDetail>({} as MovieDetail);
   loading = signal(true);
+  private movieSub?: Subscription;
   constructor(private route: ActivatedRoute, private movieService: MovieService) {}
+  ngOnDestroy(): void {
+    this.unsubscribe();
+  }
   ngOnInit(): void {
+    this.getMovie();
+  }
+  private unsubscribe(): void{
+    this.movieSub?.unsubscribe();
+  }
+  private getMovie(){
+    this.unsubscribe();
     const id = this.route.snapshot.paramMap.get('id');
-    this.movieService.getMovieDetail(id!).subscribe(response => {
+    this.movieSub = this.movieService.getMovieDetail(id!).subscribe(response => {
       if(response.ok){
         this.movieState.set(response.data);
       }
